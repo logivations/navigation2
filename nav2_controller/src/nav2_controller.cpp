49b547db96cf57001d4f0440b196f514c78adaa0
rdeniza <deniz.aksoy@logivations.com>
2020-11-04 16:58:12 +0100

    correct working params

--- nav2_controller/src/nav2_controller.cpp
+++ nav2_controller/src/nav2_controller.cpp
@@ -346,7 +346,9 @@ void ControllerServer::computeAndPublishVelocity()
 {
   geometry_msgs::msg::PoseStamped pose;
 
-  pose = odom_sub_->getPoseStamped();
+    if (!getRobotPose(pose)) {
+        throw nav2_core::PlannerException("Failed to obtain robot pose");
+    }
 
   if (!progress_checker_->check(pose)) {
     throw nav2_core::PlannerException("Failed to make progress");
@@ -416,7 +418,9 @@ bool ControllerServer::isGoalReached()
 {
   geometry_msgs::msg::PoseStamped pose;
 
-  pose = odom_sub_->getPoseStamped();
+    if (!getRobotPose(pose)) {
+        return false;
+    }
 
   nav_2d_msgs::msg::Twist2D twist = getThresholdedTwist(odom_sub_->getTwist());
   geometry_msgs::msg::Twist velocity = nav_2d_utils::twist2Dto3D(twist);
