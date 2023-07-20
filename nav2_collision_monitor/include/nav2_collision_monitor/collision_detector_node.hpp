b5fe79f9410ad29bea61f27357512eeadcedb425
Tony Najjar <tony.najjar@logivations.com>
2023-07-20 12:05:00 +0200

    address nitpick

--- nav2_collision_monitor/include/nav2_collision_monitor/collision_detector_node.hpp
+++ nav2_collision_monitor/include/nav2_collision_monitor/collision_detector_node.hpp
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NAV2_COLLISION_MONITOR__DETECTOR_NODE_HPP_
-#define NAV2_COLLISION_MONITOR__DETECTOR_NODE_HPP_
+#ifndef NAV2_COLLISION_MONITOR__COLLISION_DETECTOR_NODE_HPP_
+#define NAV2_COLLISION_MONITOR__COLLISION_DETECTOR_NODE_HPP_
 
 #include <string>
 #include <vector>
@@ -156,4 +156,4 @@ protected:
 
 }  // namespace nav2_collision_monitor
 
-#endif  // NAV2_COLLISION_MONITOR__DETECTOR_NODE_HPP_
+#endif  // NAV2_COLLISION_MONITOR__COLLISION_DETECTOR_NODE_HPP_
