17e7c59efc62c4383b327f23e4a1529390a59c9f
Johannes Plapp <johannes.plapp@logivations.com>
2025-03-07 23:45:35 +0100

    Revert "fix test failures"
    This reverts commit fe4318ea

--- nav2_behavior_tree/plugins/condition/goal_reached_condition.cpp
+++ nav2_behavior_tree/plugins/condition/goal_reached_condition.cpp
@@ -97,13 +97,13 @@ bool GoalReachedCondition::isGoalReached()
     goal_frame_transform.setOrigin(tf2::Vector3(goal.pose.position.x, goal.pose.position.y, 0.0));
     goal_frame_transform.setRotation(tf2::Quaternion(goal.pose.orientation.x, goal.pose.orientation.y, goal.pose.orientation.z, goal.pose.orientation.w));
 
-    tf2::Transform current_pose_in_goal_frame = goal_frame_transform.inverse() * tf2::Transform(tf2::Quaternion(current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w), 
+    tf2::Transform current_pose_in_goal_frame = goal_frame_transform.inverse() * tf2::Transform(tf2::Quaternion(current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, current_pose.pose.orientation.w),
     tf2::Vector3(current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z));
 
     x_in_goal_frame = fabs(current_pose_in_goal_frame.getOrigin().x());
     y_in_goal_frame = fabs(current_pose_in_goal_frame.getOrigin().y());
   }
-  
+
   double dx = goal.pose.position.x - current_pose.pose.position.x;
   double dy = goal.pose.position.y - current_pose.pose.position.y;
   double current_yaw = tf2::getYaw(current_pose.pose.orientation);
@@ -111,7 +111,7 @@ bool GoalReachedCondition::isGoalReached()
   double dangle = fabs(angles::shortest_angular_distance(goal_yaw, current_yaw));
 
   // Check conditions for x, y, and xy tolerances
-  bool within_xy_tolerance = (dx * dx + dy * dy) <= (goal_reached_tol_ * goal_reached_tol_);  
+  bool within_xy_tolerance = (dx * dx + dy * dy) <= (goal_reached_tol_ * goal_reached_tol_);
   bool within_x_tolerance = x_in_goal_frame <= goal_reached_tol_x_;
   bool within_y_tolerance = y_in_goal_frame <= goal_reached_tol_y_;
   bool within_yaw_tolerance = dangle <= goal_reached_tol_yaw_;
